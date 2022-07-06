39e69cbcd1a0de57eaafba6a2e06602fc87ab14d
Truimo <Xiaomo@20mo.cn>
2022-07-06 16:05:03 +0800

    整理文件

--- js/toptips.js
+++ js/toptips.js
@@ -2,33 +2,35 @@
  * toptips.js
  * By Truimo
 */
+
 //顶部提示
-function toptips(text, color, time){
+function toptips(text, color, time) {
     if (!time) {
         time = 2000
-    } else if (time == 'always') {
+    } else if (time === 'always') {
         time = 9999999999999;
     }
-  if(!text){
-     $('.toptips').css({'background': '#fa5151'}).text('Error:toptips() 发生错误').fadeIn("200")
-  }else{
-  if(!color){
-      $('.toptips').css({'background': '#06ae56'}).text(text).fadeIn("200")
-    } else if(color == 'danger'){
-      $('.toptips').css({'background': '#fa5151'}).text(text).fadeIn("200")
-    }else if(color == 'warn'){
-      $('.toptips').css({'background': '#ffc300'}).text(text).fadeIn("200")
-    }else if(color == 'info'){
-      $('.toptips').css({'background': '#00947e'}).text(text).fadeIn("200")
-    }else if(color == 'success'){
-      $('.toptips').css({'background': '#06ae56'}).text(text).fadeIn("200")
-    }else{
-      $('.toptips').css({'background': '#fa5151'}).text('Error:toptips() 发生错误').fadeIn("200")
+    if (!text) {
+        $('.toptips').css({'background': '#fa5151'}).text('Error:toptips() 发生错误').fadeIn("200")
+    } else {
+        if (!color) {
+            $('.toptips').css({'background': '#06ae56'}).text(text).fadeIn("200")
+        } else if (color === 'danger') {
+            $('.toptips').css({'background': '#fa5151'}).text(text).fadeIn("200")
+        } else if (color === 'warn') {
+            $('.toptips').css({'background': '#ffc300'}).text(text).fadeIn("200")
+        } else if (color === 'info') {
+            $('.toptips').css({'background': '#00947e'}).text(text).fadeIn("200")
+        } else if (color === 'success') {
+            $('.toptips').css({'background': '#06ae56'}).text(text).fadeIn("200")
+        } else {
+            $('.toptips').css({'background': '#fa5151'}).text('Error:toptips() 发生错误').fadeIn("200")
+        }
     }
-  }
-  $('body').css({ 'pointer-events': 'none' })
-  close(time)
+    $('body').css({'pointer-events': 'none'})
+    close(time)
 }
+
 // 关闭顶部提示
 function close(time) {
     setTimeout(function () {
